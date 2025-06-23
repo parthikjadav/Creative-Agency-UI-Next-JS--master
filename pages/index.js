a893032ccc62fbc773bbcac3a29b6905f409d435
parthikjadav <149650443+parthikjadav@users.noreply.github.com>
2025-06-23 15:05:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
     <>
       <Head>
     <meta name="google-site-verification" content="DjoRr8yGxh-Ho776gqKIlPeOjjGIFeIUXSzgGUBSy-4" />
+    <meta name="msvalidate.01" content="F658F7DD50506D3B863BEE772B78DDEB" />
         <title>Home - CGO</title>
       </Head>
       <Hero />
